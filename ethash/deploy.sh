db75e3f076fba5e8643adf6c7b518d24b4891182
Andre Santana <andre.s@lyncas.net>
2023-05-23 11:07:08 -0300

    mudança protocolo validacao

--- ethash/deploy.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f namespace/
-kubectl apply -f secrets/
-kubectl apply -f configmap/
-kubectl apply -f services/
-kubectl apply -f deployments/
-kubectl apply -f statefulsets/
-kubectl apply -f ingress/
--- ethash/remove.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete -f ingress/
-kubectl delete -f statefulsets/
-kubectl delete -f deployments/
-kubectl delete -f secrets/
-kubectl delete -f configmap/
-kubectl delete -f services/
-kubectl delete -f namespace/
